51942d6b4cbacc056a33993a3aa4eb0db6afcd94
mac <mac@macdeMacBook-Air.local>
2018-02-04 21:34:26 +0800

    init

---
+++ ShadowPath/shadowsocks-libev/src/includeobfs.h
@@ -0,0 +1,14 @@
+#ifndef _INCLUDEOBFSOBFS_H
+#define _INCLUDEOBFSOBFS_H
+
+#include "obfs/auth_chain.h"
+#include "obfs/auth.h"
+#include "obfs/tls1.2_ticket.h"
+#include "obfs/verify.h"
+#include "obfs/http_simple.h"
+#include "obfs/obfsutil.h"
+#include "obfs/base64.h"
+#include "obfs/crc32.h"
+#include "obfs/obfs.h"
+
+#endif // _INCLUDEOBFSOBFS_H
